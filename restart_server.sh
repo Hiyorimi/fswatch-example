d6af0d0c7c9d3ce6cd92385dc9a5dd963e4ab2f9
sobolevn <mail@sobolevn.me>
2016-07-06 19:28:41 +0300

    Fixed all issues found by `shellcheck`.
    Ignoring to of them just to fit the logic.
    Also added code-highlighting information to `readme.md`

--- restart_server.sh
+++ restart_server.sh
@@ -1,15 +1,17 @@
+#!/usr/bin/env bash
+
 server_file="file.ext"
-start_node_cmd="nohup interpreter $server_file > server.out &"
-echo $start_server_cmd
+start_node_cmd="nohup interpreter $server_file > server.out &"  # shellcheck disable=2034
+echo "$start_server_cmd"  # shellcheck disable=2154
  
 echo 'There is a change in file, restarting node'
-ps | grep "[i]nterpreter $server_file$" | awk '
+pgrep "[i]nterpreter $server_file$" | awk '
 {
     if($1!="") {
         print $1;
         system("kill " $1)
     }
-}' 
- 
+}'
+
 echo "starting server"
 eval "$start_server_cmd"
