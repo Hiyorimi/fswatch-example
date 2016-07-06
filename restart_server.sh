1ef9396e038ee74ac229b3353f7fde6deb7a12ea
Hiyorimi <playittodeath@gmail.com>
2016-07-06 20:32:32 +0400

    Update restart_server.sh

--- restart_server.sh
+++ restart_server.sh
@@ -4,7 +4,7 @@ server_file="file.ext"
 start_node_cmd="nohup interpreter $server_file > server.out &"  # shellcheck disable=2034
 echo "$start_server_cmd"  # shellcheck disable=2154
  
-echo 'There is a change in file, restarting node'
+echo 'There is a change in file, starting node'
 pgrep "[i]nterpreter $server_file$" | awk '
 {
     if($1!="") {
